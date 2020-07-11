e3d25987d1f53ca90e8275e1e50976dd4025918e
AhmedNawshad <ahmednawshad123@gmail.com>
2020-07-11 17:56:34 +0530

    Updates

---
+++ precache-manifest.af6193aa76f2f72f0216471ea7b5fa0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a608e8d18c3121a1a4b00e0b97c88ee6",
+    "url": "/react-todo.git/index.html"
+  },
+  {
+    "revision": "1c6bcd619db65d85b59b",
+    "url": "/react-todo.git/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2b65bea7a767f50311db",
+    "url": "/react-todo.git/static/css/main.b86275ba.chunk.css"
+  },
+  {
+    "revision": "1c6bcd619db65d85b59b",
+    "url": "/react-todo.git/static/js/2.4ab5df5a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo.git/static/js/2.4ab5df5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b65bea7a767f50311db",
+    "url": "/react-todo.git/static/js/main.743a44a1.chunk.js"
+  },
+  {
+    "revision": "0b1436d560d79af2800c",
+    "url": "/react-todo.git/static/js/runtime-main.ad53bb4e.js"
+  }
+]);
